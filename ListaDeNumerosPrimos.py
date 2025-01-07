e578daf87cc76bc489010bb0f8b26704bd8864f3
Rad <radixan85@gmail.com>
2025-01-07 10:48:26 -0300

    Add files via upload

---
+++ ListaDeNumerosPrimos.py
@@ -0,0 +1,15 @@
+primos = []
+def es_primo(i):
+	cantidadDiv=0
+	for ii in range(1,i+1):
+		if i%ii == 0:
+			cantidadDiv+=1
+	if cantidadDiv == 2:
+		return True
+	return False
+	
+
+for i in range (1,100 +1):
+	if es_primo(i):
+		primos.append(i)
+print(primos)
